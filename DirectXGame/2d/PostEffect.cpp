31b1c2f73ff335a753a43bc26478664d76b9aaae
GC1A_17_Horton <k020g1521@g.neec.ac.jp>
2022-06-10 06:34:20 +0900

    03_03の資料を完成させました。 確認された実行画面は期待される結果と一致します。

--- DirectXGame/2d/PostEffect.cpp
+++ DirectXGame/2d/PostEffect.cpp
@@ -1,4 +1,5 @@
 #include "PostEffect.h"
+#include "WinApp.h"
 
 #include <d3dx12.h>
 
@@ -13,6 +14,75 @@ PostEffect::PostEffect() : Sprite(
 	false, // Left/Right inversion flag
 	false) // Up/Down inversion flag
 {
+}
+
+void PostEffect::Initialize()
+{
+	HRESULT result;
+
+	// Base class initialization
+	Sprite::Initialize();
+
+	// Texture resource settings
+	CD3DX12_RESOURCE_DESC texresDesc = CD3DX12_RESOURCE_DESC::Tex2D(
+		DXGI_FORMAT_R8G8B8A8_UNORM,
+		WinApp::window_width,
+		(UINT)WinApp::window_height,
+		1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET
+	);
+
+	// Texture Buffer generation
+	result = device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_CPU_PAGE_PROPERTY_WRITE_BACK, D3D12_MEMORY_POOL_L0),
+		D3D12_HEAP_FLAG_NONE,
+		&texresDesc,
+		D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+		nullptr,
+		IID_PPV_ARGS(&texBuff));
+
+	assert(SUCCEEDED(result));
+
+	// Clear texture in red
+	//Number of pixels (1280 x 720 = 921600 pixels)
+	const UINT pixelCount = WinApp::window_width * WinApp::window_height;
+
+	// Data size for one line of image
+	const UINT rowPitch = sizeof(UINT) * WinApp::window_width;
+
+	// Data size of the whole image
+	const UINT depthPitch = rowPitch * WinApp::window_height;
+
+	// Image
+	UINT* img = new UINT[pixelCount];
+	for (int i = 0; i < pixelCount; i++) { img[i] = 0xff0000ff; }
+
+	// Transfer data to texture buffer
+	result = texBuff->WriteToSubresource(0, nullptr, img, rowPitch, depthPitch);
+	assert(SUCCEEDED(result));
+	delete[] img;
+
+	// Descriptor heap setting for SRV
+	D3D12_DESCRIPTOR_HEAP_DESC srvDescHeapDesc = {};
+	srvDescHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	srvDescHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	srvDescHeapDesc.NumDescriptors = 1;
+
+	// Generate descriptor heap for SRV
+	result = device->CreateDescriptorHeap(&srvDescHeapDesc, IID_PPV_ARGS(&descHeapSRV));
+	assert(SUCCEEDED(result));
+
+	// SRV setting
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{}; // Configuration settings
+	srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D; // 2D texture
+	srvDesc.Texture2D.MipLevels = 1;
+
+	// Create SRV in descriptor heap
+	device->CreateShaderResourceView(texBuff.Get(), // Buffer associated with view
+		&srvDesc,
+		descHeapSRV->GetCPUDescriptorHandleForHeapStart()
+	);
 }
 
 void PostEffect::Draw(ID3D12GraphicsCommandList* cmdList)
@@ -43,13 +113,15 @@ void PostEffect::Draw(ID3D12GraphicsCommandList* cmdList)
 	// Vertex buffer settings
 	cmdList->IASetVertexBuffers(0, 1, &this->vbView);
 
-	ID3D12DescriptorHeap* ppHeaps[] = { descHeap.Get() };
+	//ID3D12DescriptorHeap* ppHeaps[] = { descHeap.Get() };
+	ID3D12DescriptorHeap* ppHeaps[] = { descHeapSRV.Get() };
 	// Set descriptor heap
 	cmdList->SetDescriptorHeaps(_countof(ppHeaps), ppHeaps);
 	// Set constant buffer view
 	cmdList->SetGraphicsRootConstantBufferView(0, this->constBuff->GetGPUVirtualAddress());
 	// Set shader resource view
-	cmdList->SetGraphicsRootDescriptorTable(1, CD3DX12_GPU_DESCRIPTOR_HANDLE(descHeap->GetGPUDescriptorHandleForHeapStart(), this->texNumber, descriptorHandleIncrementSize));
+	//cmdList->SetGraphicsRootDescriptorTable(1, CD3DX12_GPU_DESCRIPTOR_HANDLE(descHeap->GetGPUDescriptorHandleForHeapStart(), this->texNumber, descriptorHandleIncrementSize));
+	cmdList->SetGraphicsRootDescriptorTable(1, descHeapSRV->GetGPUDescriptorHandleForHeapStart());
 	// Drawing command
 	cmdList->DrawInstanced(4, 1, 0, 0);
 }
