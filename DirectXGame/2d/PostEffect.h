13409d21fa8267c6e47a8b39e993cce85bf9fc4c
GC1A_17_Horton <k020g1521@g.neec.ac.jp>
2022-06-10 06:34:20 +0900

    03_03の資料を完成させました。 確認された実行画面は期待される結果と一致します。

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -10,7 +10,19 @@ public:
     PostEffect();
 
     /// <summary>
+    /// Initialization
+    /// </summary>
+    void Initialize();
+
+    /// <summary>
     /// Issuing drawing command
     /// </summary>
     void Draw(ID3D12GraphicsCommandList* cmdList);
+
+public: // Member Variables
+    // Texture Buffer
+    ComPtr<ID3D12Resource> texBuff;
+
+    // SRV descript heap
+    ComPtr<ID3D12DescriptorHeap> descHeapSRV;
 };
