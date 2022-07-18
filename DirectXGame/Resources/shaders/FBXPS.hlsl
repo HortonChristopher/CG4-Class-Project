321ad7fe922a46f515c5fca389a2b298628750bc
GC1A_17_Horton <k020g1521@g.neec.ac.jp>
2022-07-18 11:31:35 +0900

    04_05完成

--- DirectXGame/Resources/shaders/FBXPS.hlsl
+++ DirectXGame/Resources/shaders/FBXPS.hlsl
@@ -11,6 +11,19 @@ Texture2D<float4> tex : register(t0);
 // Sampler set in 0 slot
 SamplerState smp : register(s0);
 
+// Schlick near
+// f0 and f90 5 lerp
+// f0 : Reflectance when light is incident vertically
+// f90 : Relectance when light is incident in parallel
+// cosine : cosine of angle between two vectors (internal product value)
+float SchlickFresnel(float f0, float f90, float cosine)
+{
+	float m = saturate(1 - cosine);
+	float m2 = m * m;
+	float m5 = m2 * m2 * m;
+	return lerp(f0, f90, m5);
+}
+
 // Bidiretional Scattering Distribution Function (BSDF)
 float3 BRDF(float3 L, float3 V)
 {
@@ -21,10 +34,37 @@ float3 BRDF(float3 L, float3 V)
 	// Returns black if either is 90 degrees or higher
 	if (NdotL < 0 || NdotV < 0) { return float3(0, 0, 0); }
 
+	// Midway between light direction and camera direction (half vector)
+	float3 H = normalize(L + V);
+
+	// Dot product of normal and half vector
+	float NdotH = dot(N, H);
+
+	// Dot product of light and half vector
+	float LdotH = dot(L, H);
+
 	// Extension reflectance
 	float diffuseReflectance = 1.0f / PI;
+
+	float energyBias = 0.5f * roughness;
+
+	// Diffuse reflectance when angle is 90 degrees
+	float Fd90 = energyBias + 2 * LdotH * LdotH * roughness;
+
+	// Diffuse reflectance when entering
+	float FL = SchlickFresnel(1.0f, Fd90, NdotL);
+
+	// Diffuse reflectance when exiting
+	float FV = SchlickFresnel(1.0f, Fd90, NdotV);
+
+	float energyFactor = lerp(1.0f, 1.0f / 1.51f, roughness);
+
+	// Diffuse reflectance until entering and exiting
+	float Fd = FL * FV * energyFactor;
+
 	// Enlargement
-	float3 diffuseColor = diffuseReflectance * NdotL * baseColor * (1 - metalness);
+	//float3 diffuseColor = diffuseReflectance * NdotL * baseColor * (1 - metalness);
+	float3 diffuseColor = diffuseReflectance * Fd * baseColor * (1 - metalness); // Schlick
 
 	// Insert the specular formula here
 	float intensity = saturate(NdotV);
